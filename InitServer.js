cdea1e2d57dc1e1d524b78e95abee79fda9304c6
Zerofelx <zeroflex3@gmail.com>
2021-06-18 20:22:30 -0500

    Agregando página

--- InitServer.js
+++ InitServer.js
@@ -3,11 +3,31 @@
 //
 
 const Express = require('express');
+const Path = require('path');
 const app = Express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.sendFile(Path.join(__dirname, '/index.html'))
 })
+
+app.get('/bracket-individual', function (req, res) {
+    res.sendFile(Path.join(__dirname, '/data/brackets/individual/actual.json'))
+})
+
+app.get('/bracket-teams', function (req, res) {
+    res.sendFile(Path.join(__dirname, '/data/brackets/teams/actual.json'))
+})
+
+app.get('/teams', function (req, res) {
+    res.sendFile(Path.join(__dirname, '/data/teams/index.json'))
+})
+
+app.get('/games', function (req, res) {
+    res.sendFile(Path.join(__dirname, '/data/games.json'))
+})
+
+
+
 let port = process.env.PORT || 3000;
 app.listen(port)
 
